23ba8977d7dc9286a71f82f7fb5611f9b528762d
rbnuria <rbnuria6@gmail.com>
2025-05-12 13:28:09 +0200

    think: random move

--- src/personal/AIPlayer.cpp
+++ src/personal/AIPlayer.cpp
@@ -22,44 +22,44 @@ bool AIPlayer::move(){
 }
 
 void AIPlayer::think(color& c_piece, int& id_piece, int& dice) const{
-   // // IMPLEMENTACIÓN INICIAL DEL AGENTE
-   // // Esta implementación realiza un movimiento aleatorio.
-   // // Se proporciona como ejemplo, pero se debe cambiar por una que realice un movimiento inteligente
-   // //como lo que se muestran al final de la función.
+   // IMPLEMENTACIÓN INICIAL DEL AGENTE
+   // Esta implementación realiza un movimiento aleatorio.
+   // Se proporciona como ejemplo, pero se debe cambiar por una que realice un movimiento inteligente
+   //como lo que se muestran al final de la función.
 
-   // // OBJETIVO: Asignar a las variables c_piece, id_piece, dice (pasadas por referencia) los valores,
-   // //respectivamente, de:
-   // // - color de ficha a mover
-   // // - identificador de la ficha que se va a mover
-   // // - valor del dado con el que se va a mover la ficha.
+   // OBJETIVO: Asignar a las variables c_piece, id_piece, dice (pasadas por referencia) los valores,
+   //respectivamente, de:
+   // - color de ficha a mover
+   // - identificador de la ficha que se va a mover
+   // - valor del dado con el que se va a mover la ficha.
 
-   // // El id de mi jugador actual.
-   // int player = actual->getCurrentPlayerId();
+   // El id de mi jugador actual.
+   int player = actual->getCurrentPlayerId();
 
-   // // Vector que almacenará los dados que se pueden usar para el movimiento
-   // vector<int> current_dices;
-   // // Vector que almacenará los ids de las fichas que se pueden mover para el dado elegido.
-   // vector<tuple<color, int>> current_pieces;
+   // Vector que almacenará los dados que se pueden usar para el movimiento
+   vector<int> current_dices;
+   // Vector que almacenará los ids de las fichas que se pueden mover para el dado elegido.
+   vector<tuple<color, int>> current_pieces;
 
-   // // Se obtiene el vector de dados que se pueden usar para el movimiento
-   // current_dices = actual->getAvailableNormalDices(player);
-   // // Elijo un dado de forma aleatoria.
-   // dice = current_dices[rand() % current_dices.size()];
+   // Se obtiene el vector de dados que se pueden usar para el movimiento
+   current_dices = actual->getAvailableNormalDices(player);
+   // Elijo un dado de forma aleatoria.
+   dice = current_dices[rand() % current_dices.size()];
 
-   // // Se obtiene el vector de fichas que se pueden mover para el dado elegido
-   // current_pieces = actual->getAvailablePieces(player, dice);
+   // Se obtiene el vector de fichas que se pueden mover para el dado elegido
+   current_pieces = actual->getAvailablePieces(player, dice);
 
-   // // Si tengo fichas para el dado elegido muevo una al azar.
-   // if (current_pieces.size() > 0){
-   //    int random_id = rand() % current_pieces.size();
-   //    id_piece = get<1>(current_pieces[random_id]); // get<i>(tuple<...>) me devuelve el i-ésimo
-   //    c_piece = get<0>(current_pieces[random_id]); // elemento de la tupla
-   // }
-   // else{
-   //    // Si no tengo fichas para el dado elegido, pasa turno (la macro SKIP_TURN me permite no mover).
-   //    id_piece = SKIP_TURN;
-   //    c_piece = actual->getCurrentColor(); // Le tengo que indicar mi color actual al pasar turno.
-   // }
+   // Si tengo fichas para el dado elegido muevo una al azar.
+   if (current_pieces.size() > 0){
+      int random_id = rand() % current_pieces.size();
+      id_piece = get<1>(current_pieces[random_id]); // get<i>(tuple<...>) me devuelve el i-ésimo
+      c_piece = get<0>(current_pieces[random_id]); // elemento de la tupla
+   }
+   else{
+      // Si no tengo fichas para el dado elegido, pasa turno (la macro SKIP_TURN me permite no mover).
+      id_piece = SKIP_TURN;
+      c_piece = actual->getCurrentColor(); // Le tengo que indicar mi color actual al pasar turno.
+   }
 
    
    // El siguiente código se proporciona como sugerencia para iniciar la implementación del agente.
