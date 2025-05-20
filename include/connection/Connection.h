88aa2ff8d6e9e4f01571e1bf98cc62b75d9a89f9
Juan Luis <jlsuarezdiaz@ugr.es>
2025-05-20 11:42:28 +0200

    Ninja update

--- include/connection/Connection.h
+++ include/connection/Connection.h
@@ -14,7 +14,7 @@ using namespace std;
 
 // Para control de versiones y obligar a actualizar a versiones incompatibles con el online.
 # define NINJA_VERSION 1
-# define ONLINE_VERSION 1
+# define ONLINE_VERSION 2
 
 enum MessageKind{
    NOP = 0,
@@ -296,7 +296,7 @@ public:
  */
 class MasterServer{
 private:
-   static const int MAX_ALLOWED_NINJA_GAMES = 10;
+   static const int MAX_ALLOWED_NINJA_GAMES = 20;
    bool is_running;
 
    int listener_port;
